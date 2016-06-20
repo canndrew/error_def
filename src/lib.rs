600220f96c35acf0dc36d36a51c79aa2d612f46c
Andrew Cann <shum@canndrew.org>
2016-06-20 16:22:34 +0800

    Fix warning

--- src/lib.rs
+++ src/lib.rs
@@ -40,7 +40,7 @@ struct VariantDef {
   long_description: Option<LongDescription>,
 }
 
-fn expand_error_def<'c>(cx: &mut ExtCtxt, sp: Span, type_name: ast::Ident, tokens: Vec<TokenTree>) -> Box<MacResult + 'c> {
+fn expand_error_def<'c>(cx: &'c mut ExtCtxt, sp: Span, type_name: ast::Ident, tokens: Vec<TokenTree>) -> Box<MacResult + 'c> {
   let mut parser = parse::tts_to_parser(cx.parse_sess(), tokens, cx.cfg());
 
   let mut items: Vec<P<ast::Item>> = Vec::new();
